cf29f3ead26bcaa84935bf34604e9816371ca28e
Sean Steimer <ssteimer@adobe.com>
2025-12-12 13:45:30 -0800

    better default location for header and footer

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -302,7 +302,7 @@ function handleStickyNav(navWrapper, navNotification, navUtility) {
 export default async function decorate(block) {
   // load nav as fragment
   const navMeta = getMetadata('nav');
-  const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
+  const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/en/navs/nav';
   const fragment = await loadFragment(navPath);
 
   // decorate nav DOM
